c5d3a1a94bd0f03d87c3856f1f949bcb9f7876d8
Luca Minudel <mymailgateway-github@yahoo.it>
2017-10-30 17:02:46 +0000

    Removed unnecessary input from the acceptance tests.

--- 2a-Latent-code/TyresDegradationSimulator.Tests/SimulationTest.cs
+++ 2a-Latent-code/TyresDegradationSimulator.Tests/SimulationTest.cs
@@ -31,7 +31,7 @@ namespace TyresDegradationSimulator.Console.AcceptanceTests
         [Test]
         public void VettelMonza2016()
         {
-            var output = RunProgram("81.972", "0.081", "0.05", "0");
+            var output = RunProgram("81.972", "0.081");
             Assert.That(output, Does.EndWith("Lap 5: 82.377\n  Lap 6: 82.458\n  Lap 7: 82.539\n  Lap 8: 82.620\n  Lap 9: 82.701\n"));
         }
 
@@ -39,7 +39,7 @@ namespace TyresDegradationSimulator.Console.AcceptanceTests
         [Test]
         public void VettelMonza2016_UsingStoredParameters()
         {
-            RunProgram("81.972", "0.081", "0.05", "0");
+            RunProgram("81.972", "0.081");
             var output = RunProgram("", "", "", "0");
             Assert.That(output, Does.EndWith("Lap 5: 82.377\n  Lap 6: 82.458\n  Lap 7: 82.539\n  Lap 8: 82.620\n  Lap 9: 82.701\n"));
         }
@@ -48,7 +48,7 @@ namespace TyresDegradationSimulator.Console.AcceptanceTests
         public void RaikkonenMonza2016()
         {
               
-            var output = RunProgram("82.065", "0.081", "0.05", "9");
+            var output = RunProgram("82.065", "0.081");
             Assert.That(output, Does.EndWith("Lap 5: 82.470\n  Lap 6: 82.551\n  Lap 7: 82.632\n  Lap 8: 82.713\n  Lap 9: 82.794\n"));
         }
 
