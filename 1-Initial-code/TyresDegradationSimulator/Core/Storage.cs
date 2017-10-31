be78891c9042898bb8cc5ea2738da87348692581
Luca Minudel <mymailgateway-github@yahoo.it>
2017-10-31 12:03:53 +0000

    Added comments and an exception to signal an unknown file format.

--- 1-Initial-code/TyresDegradationSimulator/Core/Storage.cs
+++ 1-Initial-code/TyresDegradationSimulator/Core/Storage.cs
@@ -5,6 +5,13 @@ namespace TyresDegradationSimulator.Core
 {
     public class Storage
     {
+        // This file storage is used to simulate a large SQL db storage that makes it extremely
+        // slow (because of the quantity of data), or difficult (because of the number of clients
+        // affected) or impossible (because of irreversible schema changes) to roll back to a 
+        // previous version.
+        // It is an example of a change that breaks backwards compatibility that is impractical to 
+        // rollback as part of an automated remediation plan.
+
         private string _fileName;
 
         public Storage(string db)
@@ -25,6 +32,11 @@ namespace TyresDegradationSimulator.Core
                 {
                     var idealLapTime = fileReader.ReadDecimal();
                     var degradationPerLap = fileReader.ReadDecimal();
+
+                    if (fileReader.BaseStream.Length != fileReader.BaseStream.Position)
+                    {
+                        throw new InvalidOperationException("Unknown db format.");
+                    }
 
                     return new TyreDegradationParameters(idealLapTime, degradationPerLap);
 
